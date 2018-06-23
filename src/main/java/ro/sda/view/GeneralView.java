5819284b724c65f3a8c07a890fbd8a9274a55517
Jbanca George <gjbanca10@gmail.com>
2018-06-23 14:49:38 +0300

    Merge branch 'master' of https://github.com/george-jbanca/SDA-Shop
    # Conflicts:
    #	src/main/java/ro/sda/Main.java
    #	src/main/java/ro/sda/repository/impl/OrderRepositoryImpl.java
    #	src/main/java/ro/sda/repository/impl/ProductRepositoryImpl.java
    #	src/main/java/ro/sda/repository/impl/StockRepositoryImpl.java

--- src/main/java/ro/sda/view/GeneralView.java
+++ src/main/java/ro/sda/view/GeneralView.java
@@ -43,7 +43,7 @@ public class GeneralView implements MyMarketView {
 //                stockView.displayOptions();
                 return;
             case 4:break;
-            default:return;
+            default:break;
         }
     }
 
